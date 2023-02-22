c02f15e325dfb5a9e77df4bfc880b506cc390266
vijay kumar <308vijaykumar@gmail.com>
2023-02-23 01:02:09 +0530

    home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import Heading from '../../Components/Heading/Heading'
+import "./home.css";
+import profilePic from "../../Images/profile_pic.jpg";
+import {MdVerified} from "react-icons/md";
 
 export const Home = () => {
   return (
     <>
-    <Heading title="Home" />
-    <div>Home</div>
+      <div className='homePage'>
+        <div className='profileSection'>
+          <div className='profileContainer'>
+            <div className='profileContant'>
+              <div className='profileImage'>
+                <img src={profilePic} alt="profile-pic" />
+              </div>
+
+              <div className='profileDetials'>
+                <h2 className="name">Vijay Kumar <span className="verifiedBadge"><MdVerified/></span> </h2>
+                <p>I'm a Web Developer Based in Amritsar.</p>
+              </div>
+            </div>
+              <div className='profileRightSide'>
+                
+                <a href="">
+                  <button className="btn linkedInButton">LinkedIn</button>
+                </a>
+
+                <a href="">
+                  <button className="btn ResumeDownload">Resume</button>
+                </a>
+
+
+              </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
