e3be105ec7c7df64e7d976e85683ffc265c497b2
vijay kumar <308vijaykumar@gmail.com>
2023-03-11 14:33:36 +0530

    hambargure menu completed

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./topbar.css";
 import {AiFillInstagram, AiFillGithub, AiFillLinkedin} from "react-icons/ai";
 import {GiHamburgerMenu} from "react-icons/gi";
 const Topbar = () => {
+    let [isHamburger, setIsHamburger] = useState(true);
+    function HandlerClick(){
+        //console.log(isHamburger);
+        setIsHamburger(!isHamburger);
+    }
   return (
     <>
         <div className='topbar'>
@@ -10,15 +15,36 @@ const Topbar = () => {
                 <div className='logo'>
                     <h1 className='logo-title'> vijay kumar </h1>
                 </div>  
-                <input type="checkbox"  name="" id="hamburgerMenu" />
                 <label htmlFor="hamburgerMenu">
-                <div className="hamburgerMenu">
-                    <GiHamburgerMenu className='hamburgerMenuIcon'/>
-                </div>
+                <input type="checkbox" hidden name="" id="hamburgerMenu" onClick={HandlerClick} />
+                    <div className="hamburgerMenu"> 
+                       { 
+                         isHamburger ? <GiHamburgerMenu className='hamburgerMenuIcon'/> : <i className="fa-solid fa-xmark hamburgerMenuIcon closedIcon"></i>
+                       } 
+                    </div>
+
+                    <div className="mobileViewSlider">
+                        <ul>
+                            <div className="iconAndLabel">
+                                <i className="fa-brands fa-github"></i>
+                                <li className='github'><a href="https://github.com/Vijaykumar308/">GitHub</a></li>
+                            </div>
+
+                            <div className='iconAndLabel'>
+                                <i className="fa-brands fa-linkedin"></i>
+                                <li className='linkedIn'><a href="https://www.linkedin.com/in/vijay-kumar-679ab2221/">LinkedIn</a></li>
+                            </div>
+                            <div className='iconAndLabel'>
+                                <i className="fa-brands fa-instagram"></i>
+                                <li className='instagram'><a href="https://instagram.com/mr.vijay.kumar308">Instagram</a></li>
+                            </div>
+                            <a href="">
+                                <button className='btn btn-success-100'>Download CV</button>
+                            </a>
+                        </ul>
+                    </div>    
                 </label>
 
-                <div className="mobileViewSlider">
-                </div>    
 
                 <div className='nav'>
                     <a href='https://www.instagram.com/mr.vijay.kumar308/' target={"_blank"} rel="noreferrer">
